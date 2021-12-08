b2a8c72e7846182bcc0b6142910cedf5d3b09091
Gennadiy <lozgen@ukr.net>
2021-12-08 19:59:21 +0200

    Add onToggleProp for increase and like

--- my-app/src/components/employees-add-form/employees-add-form.js
+++ my-app/src/components/employees-add-form/employees-add-form.js
@@ -12,12 +12,13 @@ class EmployeesAddForm extends Component {
 
   onValueChange = (e) => {
     this.setState({
-      [e.target.name]: e.target.value
+        [e.target.name]: e.target.value
     })
   }
 
   onSubmit = (e) => {
     e.preventDefault();
+    if (this.state.name.length < 3 || !this.state.salary) return;
     this.props.onAdd(this.state.name, this.state.salary);
     this.setState({
         name: '',
